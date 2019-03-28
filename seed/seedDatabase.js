fd372b453a13503c47a4e1e525708429cd9a8156
manoy96 <mfewcar@gmail.com>
2019-03-28 15:07:40 -0600

    updating the seedDatabase.js to insert the Rocket data

--- seed/seedDatabase.js
+++ seed/seedDatabase.js
@@ -1,19 +1,28 @@
 const fs = require('fs')
-const { GraphQLClient } = require('graphql-request')
+const {
+   GraphQLClient
+} = require('graphql-request')
 
 const client = new GraphQLClient('http://localhost:4466')
 
-const mutation = `mutation createPokemon(
-    $name: String,
-    $height: Int,
-    $weight: Int,
-    $url: String
+const mutation = `mutation addRocket(
+
+   $name: String
+   $cost: Int
+   $id: ID! @unique
+   $payloadLeo: Int
+   $payloadGto:Int
+   $kgLeo: Int
+   $kgGto: Int
 ) {
-    createPokemon(data: {
-      name: $name
-      height: $height
-      weight: $weight
-      url: $url
+    createRocket(data: {
+      name: String
+      cost: Int
+      id: ID! @unique
+      payloadLeo: Int
+      payloadGto:Int
+      kgLeo: Int
+      kgGto: Int
     })
     {
       id
@@ -24,25 +33,25 @@ const mutation = `mutation createPokemon(
 const sampleFiles = ['pokemon-data.json']
 
 async function main(inputFile) {
-  const content = fs.readFileSync(`./seed/${inputFile}`)
-  const allPokemon = JSON.parse(content)
+   const content = fs.readFileSync(`./seed/${inputFile}`)
+   const allPokemon = JSON.parse(content)
 
-  allPokemon.forEach(async item => {
-    const variables = {
-        name: item.name,
-        height: item.height,
-        weight: item.weight,
-        url: item.url,
+   allPokemon.forEach(async item => {
+      const variables = {
+         name: item.name,
+         height: item.height,
+         weight: item.weight,
+         url: item.url,
       }
-    
+
       await client
-        .request(mutation, variables)
-        .then(data => console.log(data))
-        .catch(err => console.log(`${err}`))
-  })
-  
+         .request(mutation, variables)
+         .then(data => console.log(data))
+         .catch(err => console.log(`${err}`))
+   })
+
 }
 
 for (let fileName of sampleFiles) {
-    main(fileName).catch(e => console.error(e))
+   main(fileName).catch(e => console.error(e))
 }
