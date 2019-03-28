d61be44798b6c50fd4800da3e136008d18cfeb0d
manoy96 <mfewcar@gmail.com>
2019-03-28 09:12:59 -0600

    initial

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,61 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Rocket",
+    embedded: false
+  },
+  {
+    name: "Model",
+    embedded: false
+  },
+  {
+    name: "Pokemon",
+    embedded: false
+  },
+  {
+    name: "Ability",
+    embedded: false
+  },
+  {
+    name: "Form",
+    embedded: false
+  },
+  {
+    name: "GameIndex",
+    embedded: false
+  },
+  {
+    name: "Item",
+    embedded: false
+  },
+  {
+    name: "Move",
+    embedded: false
+  },
+  {
+    name: "Species",
+    embedded: false
+  },
+  {
+    name: "Sprite",
+    embedded: false
+  },
+  {
+    name: "Stat",
+    embedded: false
+  },
+  {
+    name: "Type",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:4466`
+});
+exports.prisma = new exports.Prisma();
